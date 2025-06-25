d72964aaa5e0e543869cb5ae3fb9ec1f16b494df
Edu <eduardo.a.soares2005@gmail.com>
2025-06-25 10:21:00 -0300

    Atualização geral

--- cypress/e2e/cadastro.cy.js
+++ cypress/e2e/cadastro.cy.js
@@ -12,17 +12,60 @@ describe('Tela de cadastro', () => {
         cy.visit('/')
     });
 
-    it('Deve realizar cadastro', () => {
+    it('Deve realizar cadastro com sucesso', () => {
         let nome = `usuario${faker.person.firstName()}`
         let senha = `${faker.internet.password()}`
         inicioPage.selecionarAba('Account')
         loginPage.acessarCriarConta()
-        cadastroPage.realizarCadastro(nome,faker.person.lastName(),faker.phone.number(),faker.internet.email(nome), senha, senha)
+        cadastroPage.realizarCadastro(nome, faker.person.lastName(), faker.phone.number(), faker.internet.email(nome), senha, senha)
         inicioPage.selecionarAba('Account')
         cy.get('.r-14lw9ot > :nth-child(5) > .css-146c3p1').should('have.text', 'Logout')
+        cy.get('[data-testid="CustomerName"]').should('contain', nome)
     });
 
+    it('Deve dar erro ao tentar cadastrar sem nome', () => {
+        let nome = `usuario${faker.person.firstName()}`
+        let senha = `${faker.internet.password()}`
+        inicioPage.selecionarAba('Account')
+        loginPage.acessarCriarConta()
+        cadastroPage.realizarCadastro(' ', faker.person.lastName(), faker.phone.number(), faker.internet.email(nome), senha, senha)
+        cy.get('[data-testid="warning"] > .r-jwli3a').should('contain', 'Please enter firstName')
+    });
+
+    it('Deve dar erro ao tentar cadastrar sem sobrenome', () => {
+        let nome = `usuario${faker.person.firstName()}`
+        let senha = `${faker.internet.password()}`
+        inicioPage.selecionarAba('Account')
+        loginPage.acessarCriarConta()
+        cadastroPage.realizarCadastro(nome, ' ', faker.phone.number(), faker.internet.email(nome), senha, senha)
+        cy.get('[data-testid="warning"] > .r-jwli3a').should('contain', 'Please enter lastName')
+    });
+
+    it('Deve dar erro ao tentar cadastrar sem email', () => {
+            let nome = `usuario${faker.person.firstName()}`
+            let senha = `${faker.internet.password()}`
+            inicioPage.selecionarAba('Account')
+            loginPage.acessarCriarConta()
+            cadastroPage.realizarCadastro(nome, faker.person.lastName(), faker.phone.number(), ' ', senha, senha)
+            cy.get('[data-testid="warning"] > .r-jwli3a').should('contain', 'Please enter an email')
+        });
+
+    it  ('Deve dar erro ao tentar cadastrar sem senha', () => {
+        let nome = `usuario${faker.person.firstName()}`
+        let senha = `${faker.internet.password()}`
+        inicioPage.selecionarAba('Account')
+        loginPage.acessarCriarConta()
+        cadastroPage.realizarCadastro(nome, faker.person.lastName(), faker.phone.number(), faker.internet.email(nome), ' ', senha)
+        cy.get('[data-testid="warning"] > .r-jwli3a').should('contain', 'Please enter password')
+    });
+    
+    it('Deve dar erro ao tentar cadastrar senha e confirmação de senha diferentes', () => {
+        let nome = `usuario${faker.person.firstName()}`
+        let senha = `${faker.internet.password()}`
+        inicioPage.selecionarAba('Account')
+        loginPage.acessarCriarConta()
+        cadastroPage.realizarCadastro(nome, faker.person.lastName(), faker.phone.number(), faker.internet.email(nome), senha, 'senhaErrada')
+        cy.get('[data-testid="warning"] > .r-jwli3a').should('contain', 'Password and confirm password do not match')
+    });
 });
-
-
 
--- cypress/e2e/carrinho.cy.js
+++ cypress/e2e/carrinho.cy.js
@@ -1,18 +1,21 @@
 /// <reference types = 'cypress'/>
-import inicioPage from "../support/pageObjects/inicioPage";
-
-beforeEach(() => {
-  cy.setCookie('ebacStoreVersion', 'v2', { domain: 'lojaebac.ebaconline.art.br' })
-  cy.visit('/')
-})
 
 describe('Carrinho de itens', () => {
+
+  beforeEach(() => {
+  cy.setCookie('ebacStoreVersion', 'v2', { domain: 'lojaebac.ebaconline.art.br' })
+  cy.visit('/')
+});
+
+afterEach(() => {
+  cy.get('[data-testid="remove"]').click()
+});
   
   it('Deve adicionar produtos ao carrinho com sucesso', () => {
     cy.fixture('Usuario').then((dados) => {
       cy.fazerLogin(dados.email, dados.senha)
-      cy.AdicionarCarrinho('tênis')
-      cy.get('[data-testid="productName"]').should('have.text', 'tênis')
+      cy.AdicionarCarrinho('Tênis')
+      cy.get('[data-testid="productName"]').should('contain', 'Tênis')
 
     });  
   });
@@ -21,9 +24,11 @@ describe('Carrinho de itens', () => {
     cy.fixture('Usuario').then((dados) => {
       cy.fazerLogin(dados.email, dados.senha)
       cy.AdicionarCarrinho('tênis')
-      let valor = cy.get('[style="color: rgb(51, 144, 124); font-size: 16px; margin-right: 10px; font-family: Montserrat-SemiBold;"]')
-      cy.get('.r-1mmae3n > :nth-child(2) > [data-testid="price"]').should('equal', valor)
-
+      cy.get('[style="color: rgb(51, 144, 124); font-size: 16px; margin-right: 10px; font-family: Montserrat-SemiBold;"]')
+      .invoke('text').then((valor)=>{
+      cy.get('[data-testid="total"]').should('have.text', valor)
+      });
+      
     });
   });
 });
